20f45f91444c1a4b9b946d9fcba6fd19f5796531
sbx_user1051 <sbx_user1051@169.254.27.174>
2024-12-15 20:05:25 +0000

    Sun Dec 15 20:05:25 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHK;
 
+import befaster.solutions.HLO.HelloSolution;
 import befaster.solutions.SUM.SumSolution;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,5 +11,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.Matchers.equalTo;
 
 public class CheckoutSolutionTest {
+    private CheckoutSolution checkoutMain;
+
+    @BeforeEach
+    public void setUp() {
+        checkoutMain = new CheckoutSolution();
+    }
+
+    @Test
+    public void testValidInputWithoutDiscount(){
+        int expected = 115;
+        int actual = checkoutMain.checkout("ABCD");
+        assertEquals(expected, actual, "Expected Price should be 115");
+    }
+
+    @Test
+    public void testInvalidInputWithoutDiscount(){
+        int expected = 105;
+        int actual = checkoutMain.checkout("ABCD");
+        assertEquals(expected, actual, "Expected Price should be 115");
+    }
 }
+
 
