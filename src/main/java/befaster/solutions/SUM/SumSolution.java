5f9c4d68277627e7f8084fa3da708b33915d9904
sbx_user1051 <sbx_user1051@169.254.27.174>
2024-12-15 19:15:02 +0000

    Sun Dec 15 19:15:02 UTC 2024

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -6,7 +6,14 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        if (x<=0 && x>=100 && y<=0 && y>=100){
+            return 0;
+        }
+        else{
+            return x+y;
+        }
+        //throw new SolutionNotImplementedException();
     }
 
 }
+
