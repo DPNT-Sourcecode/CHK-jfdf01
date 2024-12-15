50c9cf429500d94f0c85ccd1b28e3dd89d649712
sbx_user1051 <sbx_user1051@169.254.27.174>
2024-12-15 20:02:25 +0000

    Sun Dec 15 20:02:25 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,61 @@
 package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
+import com.google.gson.internal.bind.JsonAdapterAnnotationTypeAdapterFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        if(skus == null || !skus.matches("[A-D]*")){
+            return -1;
+        }
+
+        Map<Character, Integer> itemPrices = Map.of(
+                'A', 50,
+                'B', 30,
+                'C', 20,
+                'D', 15
+        );
+
+        Map<Character, Discount> discountOffers = new HashMap<>();
+        discountOffers.put('A', new Discount(3, 130));
+        discountOffers.put('B', new Discount(2, 45));
+
+        Map<Character, Integer> skuCounts = new HashMap<>();
+
+        for(char sku: skus.toCharArray()){
+            skuCounts.put(sku, skuCounts.getOrDefault(sku,0) +1);
+        }
+
+        int totalPrice = 0;
+        for(Map.Entry<Character, Integer> entry: skuCounts.entrySet()){
+            char sku = entry.getKey();
+            int count = entry.getValue();
+
+            if(discountOffers.containsKey(sku)) {
+                Discount discount = discountOffers.get(sku);
+                totalPrice += (count / discount.bundleSize) * discount.bundlePrice;
+                totalPrice += (count % discount.bundleSize) *  itemPrices.get(sku);
+            }
+            else {
+                totalPrice += count * itemPrices.get(sku);
+            }
+        }
+
+        return totalPrice;
+        //throw new SolutionNotImplementedException();
+    }
+
+    static class Discount{
+        int bundleSize;
+        int bundlePrice;
+
+        Discount(int bundleSize, int bundlePrice){
+            this.bundleSize = bundleSize;
+            this.bundlePrice = bundlePrice;
+        }
     }
 }
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,2 +1,14 @@
-package befaster.solutions.CHK;public class CheckoutSolutionTest {
+package befaster.solutions.CHK;
+
+import befaster.solutions.SUM.SumSolution;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionTest {
 }
+
